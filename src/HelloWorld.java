05069cda5bcef6076a713ac265a7c492f011cd62
ktalimaa <kristeltalimaa@gmail.com>
2022-09-14 19:25:17 +0300

    Helloworld done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
+/**
+ * The first hello world program
+ *
+ * @author Kristel Talimaa
+ */
 public class HelloWorld {
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
