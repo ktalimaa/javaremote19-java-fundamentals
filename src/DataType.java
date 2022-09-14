7cdbb1de19455337bda73b3b1eb17ea032bd9e3c
ktalimaa <kristeltalimaa@gmail.com>
2022-09-14 20:06:07 +0300

    Helloworld done

---
+++ src/DataType.java
@@ -0,0 +1,33 @@
+/**
+ * Examples of Primitive datatype
+ *
+ * @author Kristel Talimaa
+ */
+public class DataType {
+    int a = 1;
+    byte byteNumber = 120;
+    long longNumber = 12345678987654321L;
+    short shortNumber = -32000;
+    float decimalNumber = 2.455734895738495738495735873489574385793753984753489759834758947583974973f;
+    double doubleNumber = 3.456d;
+
+
+    //Character
+    char alpha = 'F';
+
+    //True/False
+    boolean hadLunch = true; // had lunch?
+    boolean isHoliday = false;
+
+    /* Multi-line
+    comment
+     */
+
+    int 1; // 0
+
+    float x; // 0.00f
+
+    char animal; // ''
+
+    boolean isClassTomorrow; // false
+}
