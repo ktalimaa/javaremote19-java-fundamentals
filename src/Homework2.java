f65e9cf8c6a286ed0b7eab926b260ac063ec20f8
ktalimaa <kristeltalimaa@gmail.com>
2022-09-19 22:08:03 +0300

    Switch homework added

---
+++ src/Homework2.java
@@ -0,0 +1,25 @@
+/**
+ * Write a Java program that swaps two variables
+ *
+ * @author Kristel Talimaa
+ *
+ */
+public class Homework2 {
+    public static void main(String[] args) {
+
+        String Audi = "Black";
+        switch (Audi) {
+            case "White":
+                System.out.println("I have a white Audi!");
+                break;
+            case "Blue":
+                System.out.println("I have a blue Audi!");
+                break;
+            case "Black":
+                System.out.println("I have a black Audi!");
+                break;
+        }
+    }
+}
+
+
