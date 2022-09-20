cd0566af82b7a69fab43b6ad9a4ad0ad9a9e1d70
ktalimaa <kristeltalimaa@gmail.com>
2022-09-20 21:13:23 +0300

    Statement exercises added II

---
+++ src/Homework3.java
@@ -0,0 +1,16 @@
+/**
+ * Homework 3: FizzBuzz one line code
+ *
+ * @author Kristel Talimaa
+ */
+
+public class Homework3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            System.out.println(i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz": i+ "");
+        }
+    }
+}
+
+
+
