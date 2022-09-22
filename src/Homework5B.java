74df916d3f5b5786630dd58822f0bfacefb39c11
ktalimaa <kristeltalimaa@gmail.com>
2022-09-22 21:50:35 +0300

    Homework 5B started II

--- src/Homework5B.java
+++ src/Homework5B.java
@@ -7,18 +7,18 @@ public class Homework5B {
     public static void main(String[] args) {
 
         int[] b = {1, 7, 3, 10, 9};
-            System.out.print("These are odd numbers: ");
-            for (int i = 0; i < b.length; i++) {
-                if (b[i] % 2 != 0) {
-                    System.out.print(b[i] + "\t");
+        for (int i = 0; i < b.length; i++) {
+            if (b[i] % 2 != 0) {
+                System.out.print("These are odd numbers: " + b[i] + "\t");
 
-                System.out.println("These are even number: ");
+
                 for (int j = 0; j < b.length; j++) {
                     if (b[j] % 2 == 0) {
-                        System.out.println(b[j] + "\t");
+                        System.out.println("These are even numbers: " + b[i] + "\t");
                     }
                 }
             }
         }
     }
 }
+
