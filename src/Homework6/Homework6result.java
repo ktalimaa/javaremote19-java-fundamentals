20a72ec8d4bdd1204b760f90a11ef20bb3526f4a
ktalimaa <kristeltalimaa@gmail.com>
2022-10-01 16:10:12 +0300

    Homework 6 still same

--- src/Homework6/Homework6result.java
+++ src/Homework6/Homework6result.java
@@ -10,11 +10,9 @@ import java.util.Arrays;
 public class Homework6result {
     public static void main(String[] args) {
         int[] arrayToCalculate = {1, 7, 3, 10, 9};
-
         Homework6calculator arrayFinder = new Homework6calculator();
-
         String result = arrayFinder.findSecondElement(arrayToCalculate);
-
         System.out.println(result);
+
     }
 }
