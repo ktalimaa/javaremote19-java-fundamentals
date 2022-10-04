85e3cf34e3bf49b534ad5bcd73a47ee3f3ddc109
ktalimaa <kristeltalimaa@gmail.com>
2022-10-04 21:38:33 +0300

    Live coding I homework added

--- src/homeworks/IntegerDisplay.java
+++ src/homeworks/IntegerDisplay.java
@@ -1,5 +1,7 @@
 package homeworks;
 
+import java.util.Scanner;
+
 /**
  * To read a number from user and print the same number as this:
  * e.g. if given number is 4, then print:
@@ -10,7 +12,17 @@ package homeworks;
 public class IntegerDisplay {
     public static void main(String[] args) {
 
+        System.out.println("Print number to display: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
+        System.out.println("The result of numbers: ");
+        for (int a = 0; a < n; a++) {
+            System.out.print(" ");
+            for (int b = 0; b <= a; b++) {
+                System.out.print(n + "\t");
+            }
+        }
 
 
     }
