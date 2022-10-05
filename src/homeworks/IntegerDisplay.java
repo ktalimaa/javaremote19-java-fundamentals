5013639112a9e5d1fb8d3723f947294952451238
ktalimaa <kristeltalimaa@gmail.com>
2022-10-05 18:57:22 +0300

    Homework done again

--- src/homeworks/IntegerDisplay.java
+++ src/homeworks/IntegerDisplay.java
@@ -11,7 +11,30 @@ import java.util.Scanner;
  */
 public class IntegerDisplay {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= number; i++) {  // first loop is to print number of lines/times
+            for (int j = 1; j <= i; j++) {   // second loop is to print number of digits
+                System.out.print(number);    // use just print
+            }
+            System.out.println();
+        }
+
+        // same triangle but opposite way
+        for (int i = number; i >= 1; i--) {  // first loop is to print number of lines/times
+            for (int j = i; j >= 1; j--) {   // second loop is to print number of digits
+                System.out.print(number);    // use just print
+            }
+            System.out.println();
+        }
+
+
+
+        /*
+        My version:
         System.out.println("Print number to display: ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -23,6 +46,8 @@ public class IntegerDisplay {
                 System.out.print(n + "\t");
             }
         }
+
+         */
 
 
     }
